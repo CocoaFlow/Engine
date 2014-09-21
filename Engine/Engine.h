8074017820f743b461fd21ca9bd4033ba2c08082
Paul Young <paulyoungonline@gmail.com>
2014-09-21 07:57:04 -0700

    Initial Commit

---
+++ Engine/Engine.h
@@ -0,0 +1,19 @@
+//
+//  Engine.h
+//  Engine
+//
+//  Created by Paul Young on 21/09/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Engine.
+FOUNDATION_EXPORT double EngineVersionNumber;
+
+//! Project version string for Engine.
+FOUNDATION_EXPORT const unsigned char EngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Engine/PublicHeader.h>
+
+
