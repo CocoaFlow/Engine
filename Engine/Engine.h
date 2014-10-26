6a98cdc706a67dadf9c638d4c63313bfe6f676c0
Paul Young <paulyoungonline@gmail.com>
2014-10-26 13:31:33 +0000

    Import Foundation instead of Cocoa or UIKit.

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 CocoaFlow. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Engine.
 FOUNDATION_EXPORT double EngineVersionNumber;
