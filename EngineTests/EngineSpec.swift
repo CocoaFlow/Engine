b2e0040f20937d077939f9544570f8aa9ae90479
Paul Young <paulyoungonline@gmail.com>
2014-10-26 13:54:57 +0000

    Add Quick and Nimble to workspace & test targets.
    * Make the schemes shared.
    * Replace placeholder test file.
    * Clean up framework search paths for test targets.

---
+++ EngineTests/EngineSpec.swift
@@ -0,0 +1,21 @@
+//
+//  EngineSpec.swift
+//  Engine
+//
+//  Created by Paul Young on 26/10/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+class EngineSpec: QuickSpec {
+    override func spec() {
+        describe("engine") {
+            it("tests should run") {
+                expect(true).to(equal(true));
+            }
+        }
+    }
+}
