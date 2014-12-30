99c064ece59d3adaa53b5e2e1d7b29e78bf2fbc6
Paul Young <paulyoungonline@gmail.com>
2014-12-30 10:58:50 -0800

    Fix initializers.

--- EngineTests/CallbackComponent.swift
+++ EngineTests/CallbackComponent.swift
@@ -14,14 +14,14 @@ final internal class CallbackComponent<T>: Engine.Component {
     lazy var id: Int = ObjectIdentifier(self).hashValue
     
     convenience init(_ network: Network) {
-        self.init(network)
+        self.init(network, { packet in })
     }
     
     // Mark: - Callback
     private typealias Callback = (T) -> Void
     private let callback: Callback
     
-    init(_ network: Network, callback: Callback) {
+    init(_ network: Network, _ callback: Callback) {
         self.network = network
         self.callback = callback
     }
